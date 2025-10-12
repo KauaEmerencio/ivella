e7de6f1ec3eec5339c20677fa019346aa534da8b
Kauã Lisboa Emerencio <kaualisboa2018@gmail.com>
2025-10-11 23:42:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -63,8 +63,8 @@ function initApp() {
 }
 
 function configurarMenuMobile() {
-    const hamburger = document.querySelector('.hamburger');
-    const navMenu = document.querySelector('.nav-menu');
+   const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
     
     if (hamburger && navMenu) {
         hamburger.addEventListener('click', function() {
